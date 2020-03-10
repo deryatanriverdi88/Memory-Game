a635662f41702d104ca23265c4059c9c70d97b38
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-03-10 19:39:23 -0400

    copies in state and setSelected method from Game

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,9 @@ import CardBack from './CardBack'
 export default class Card extends Component {
 
     state = {
-        cardFaceDown: true
+        cardFaceDown: true,
+        index: null, 
+        imgId: null
     }
 
     // renderCard = () => {
@@ -28,14 +30,69 @@ export default class Card extends Component {
         })
     }
 
+      
+       OpenImage(index, img){
+         console.log( "an image with a index of"+ index + " opened")
+       }
+     
+       CloseImage(key){
+         console.log("an image with a index of"+ key + " closed")
+       }
+     
+       setSelected = (e, img,index) =>{
+     
+         if(this.state.index==null)
+         { 
+           this.setState({
+             index: index, 
+             imgId: img.id, 
+             
+           })
+           
+           this.OpenImage(index, img);
+           //console.log("first image opened");
+       
+         }else{
+           
+           if(this.state.index===index)
+           {
+             return ;// it will stop working here
+           
+           } 
+     
+           this.OpenImage(index);
+     
+           if(this.state.imgId!==img.id)
+           {
+             console.log("photos are not the same ",this.state.imgId,img.id )
+     
+               this.CloseImage(this.state.index);
+               this.CloseImage(index);
+               
+           }
+           else{
+             console.log("photos are the same",this.state.imgId, img.id)
+             
+           }
+     
+           this.setState({
+             index:null, 
+             imgId: null
+           })
+     
+         }
+     
+       }
+  
+
     render() {
         const {img, index, setSelected} = this.props
         return (
             <>
              {this.state.cardFaceDown ? 
-              <CardBack img={img} index={index} setSelected={setSelected} flipOneCard={this.flipOneCard}/> : 
+              <CardBack img={img} index={index} setSelected={this.setSelected} flipOneCard={this.flipOneCard}/> : 
 
-              <CardFront img={img} index={index} setSelected={setSelected} flipOneCard={this.flipOneCard}/>
+              <CardFront img={img} index={index} />
             }
              
 {/*  
