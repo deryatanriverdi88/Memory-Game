51d61e3335f8cd660ac841367bada29a50e79cf8
kmarks2013 <kmarks2013@gmail.com>
2020-03-18 18:49:47 -0400

    changes img src to  svg  file, and changes card back class

--- src/Components/CardBack.js
+++ src/Components/CardBack.js
@@ -6,9 +6,8 @@ export default class CardBack extends Component {
     render() {
         const {img,flipOneCard} = this.props
         return (
-            <div className="card" onClick={() => flipOneCard(img)}>
-                <img className="front" 
-               src="../logo192.png" alt={`${img}`}  /> 
+            <div className="card" onClick={() => flipOneCard(img)} >
+                <img className="back" src="../svg/logo.svg" alt='logo'  /> 
            </div>
         )
     }
