c0bb693d4103ece338b4a2c4699506ae1232fcc9
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-03-17 23:01:02 -0400

    adds timer

---
+++ src/Components/Timer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export default class Timer extends Component {
+
+    state ={ timer: 0}
+    
+    start =
+        setInterval(
+            () => this.setState({ timer: this.state.timer + 1}),
+              1000
+        );
+
+    stop = () => {
+        clearInterval(this.start)
+    }
+
+    componentDidMount= () =>{
+        if(this.props.gameStatus === "play"){
+            return this.start
+         }
+    }
+       
+    componentDidUpdate= () => {
+        if(this.props.gameStatus ==="winner"){
+            clearInterval(this.start)
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.gameStatus === "play"  ? <h1>Time : {this.state.timer}</h1> : <h1> Your time was : {this.state.timer} </h1>}
+            </div>
+        )
+    }
+}
