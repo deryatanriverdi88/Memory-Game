a25d31a457fd0b16d30d8bfc22001dcc410a7c57
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-03-17 22:40:14 -0400

    removes moves from state in CardList

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -7,8 +7,7 @@ export default class CardList extends Component {
         firstCard: null, 
         secondCard: null,
         images: [...this.props.images],
-        matchedPairs: [],
-        moves: 0
+        matchedPairs: []
     }
 
     choosenCards = (img) => {
