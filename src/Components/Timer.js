37a234eb4bf6a013a2d384b02ffca63758defaed
kmarks2013 <kmarks2013@gmail.com>
2020-03-19 22:55:01 -0400

    moves logic from timer back into Timer

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -2,30 +2,30 @@ import React, { Component } from 'react'
 
 export default class Timer extends Component {
 
-    // state ={ timer: 0}
+    state ={ timer: 0}
     
-    // start =
-    //     setInterval(
-    //         () => this.setState({ timer: this.state.timer + 1}),
-    //           1000
-    //     );
+    start =
+        setInterval(
+            () => this.setState({ timer: this.state.timer + 1}),
+              1000
+        );
 
-    // componentDidMount= () =>{
-    //     if(this.props.gameStatus === "play"){
-    //         return this.start
-    //      }
-    // }
+    componentDidMount= () =>{
+        if(this.props.gameStatus === "play"){
+            return this.start
+         }
+    }
        
-    // componentDidUpdate= () => {
-    //     if(this.props.gameStatus ==="winner"){
-    //         clearInterval(this.start)
-    //     }
-    // }
+    componentDidUpdate= () => {
+        if(this.props.gameStatus ==="winner"){
+            clearInterval(this.start)
+        }
+    }
 
     render() {
         return (
             <div className="timer">
-                {this.props.gameStatus === "play"  ? <h1>Time : {this.props.timer}</h1> : <h1> Your time was : {this.props.timer} </h1>}
+                {this.props.gameStatus === "play"  ? <h1>Time : {this.state.timer} seconds</h1> : <h1> Your time was : {this.state.timer} seconds</h1>}
             </div>
         )
     }
