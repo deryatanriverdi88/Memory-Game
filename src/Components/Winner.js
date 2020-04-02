72397496f61802c9c09ee1b5de21711d1f33b350
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-01 22:38:24 -0400

    converts Winner from functional component to class components, adds state of username

--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
-export default function Winner({moves, resetGame }) {
+export default class Winner extends React.Component {
+   state = {
+       username: ""
+   }
 
-    return (
-        <div className="winner" >
-            <h1 className="moves" > Moves : {moves}</h1>
-            <h1>
-                You Won!!!!  
-            </h1>
-            <img src="../images/happy.gif" /> 
-            <button className='button' onClick={()=> resetGame()}>Reset</button>
-        </div>
-    )
+    render(){
+        const {moves, resetGame} = this.props
+        return (
+            <div className="winner" >
+                <h1 className="moves" > Moves : {moves}</h1>
+                <h1>
+                    You Won!!!!
+                </h1>
+                <img src="../images/happy.gif"/>
+                <button className='button' onClick={()=> resetGame()}>Reset</button>
+            </div>
+        )
+    }
 }
