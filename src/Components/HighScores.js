c60889cfeaf5af15b12582f412620e7b4475390c
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-06 23:17:47 -0400

    adds HighScore component

---
+++ src/Components/HighScores.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class HighScores extends Component {
+    render() {
+        return (
+            <div>
+              
+            </div>
+        )
+    }
+}
+
