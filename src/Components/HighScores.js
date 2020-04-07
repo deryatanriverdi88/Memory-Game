cc2c17bec96dce59c94a8f7b4b23e5b862035579
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-06 23:18:45 -0400

    adds highScore to state

--- src/Components/HighScores.js
+++ src/Components/HighScores.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 export default class HighScores extends Component {
+
+    state = {
+        highScores: []
+    }
+
     render() {
         return (
             <div>
