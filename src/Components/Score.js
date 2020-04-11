5013db5d016ff978f5aae78a77357d6c038b5116
kmarks2013 <kmark2013@gmail.com>
2020-04-11 19:07:42 -0400

    adds scoreboard classname and capitalizes score and current streak

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -5,16 +5,16 @@ export default class Score extends Component {
         return (
             <div>
                 {this.props.gameStatus === "winner" ?
-                    <div>
+                    <div className='scoreboard'>
                     <p>Score : {this.props.score}</p>
                     <p>Bonus: {this.props.finalScore - this.props.score }</p>
                     <p>Total : {this.props.finalScore}</p>
                     </div>
                     :
-                    <>
-                    <p> score: {this.props.score} </p>
-                    <p> current streak: {this.props.streak} </p>
-                    </>}
+                    <div className='scoreboard'>
+                    <p> Score: {this.props.score} </p>
+                    <p> Current Streak: {this.props.streak} </p>
+                    </div>}
             </div>
         )
     }
