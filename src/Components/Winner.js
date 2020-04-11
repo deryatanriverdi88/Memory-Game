e2e8f1efab9d4d2f5e8ba844545027abd168d684
kmarks2013 <kmark2013@gmail.com>
2020-04-11 19:09:29 -0400

    remvoes username render from Winner component

--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -60,7 +60,6 @@ export default class Winner extends React.Component {
                 <h1>
                     You Won!!!!
                 </h1>
-                <p>Name : {this.state.username}</p>
                 <img src="../images/happy.gif"/>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="username">Your name:</label>
