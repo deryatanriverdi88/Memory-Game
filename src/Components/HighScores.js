112cc4958fbd59a95b13ec7e6bc06e90213f801e
kmarks2013 <kmarks2013@gmail.com>
2020-04-12 01:44:26 -0400

    adds scoreIndex and newScores to state

--- src/Components/HighScores.js
+++ src/Components/HighScores.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class HighScores extends Component {
 
     state = {
-        highScores: []
+        highScores: [],
+        scoreIndex: null,
+        newScores: []
+
     }
 
     componentDidMount = () => {
